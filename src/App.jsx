84af8f9d83fcebcd899ca33aab1554bee0e0520d
AsadurRubel <miarubel04@.com>
2024-04-23 03:40:24 +0600

    First day task complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './Components/CoffeeCard';
+import { useState } from 'react';
 
 function App() {
 
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
+
 console.log(coffees)
 
   return (
@@ -15,7 +20,12 @@ console.log(coffees)
 
       <div className='grid grid-cols-3 gap-5 mt-10'>
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard> )
+          coffees.map(coffee => <CoffeeCard
+             key={coffee._id}
+              coffee={coffee}
+              coffees={coffees}
+              setCoffees= {setCoffees}
+              ></CoffeeCard> )
         }
       </div>
       
--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import PropTypes from 'prop-types';
 
-const CoffeeCard = ({ coffee }) => {
-    const {_id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+    const { _id, name, quantity, supplier, taste, photo } = coffee;
     console.log(coffee)
 
-    const handleDelete = _id =>{
+    const handleDelete = _id => {
         console.log(_id)
         Swal.fire({
             title: "Are you sure?",
@@ -15,28 +17,31 @@ const CoffeeCard = ({ coffee }) => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-            
 
-            fetch(`http://localhost:5000/coffee/${_id}`,{
-                method: 'DELETE',
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if(data.deleteCount > 0){
-                      Swal.fire({
-                title: "Deleted!",
-                text: "Your coffee has been deleted.",
-                icon: "success"
-              });
-                }
-            })
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deleteCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+
+                        }
+                        const remaining = coffees.filter(cof => cof._id !== _id);
+                        setCoffees(remaining)
+                    })
 
 
             }
-          });
+        });
     }
 
 
@@ -46,24 +51,28 @@ const CoffeeCard = ({ coffee }) => {
         <div className="card card-side bg-base-100 shadow-xl mb-4">
             <figure><img className="w-full" src={photo} alt="Movie" /></figure>
             <div className="card-body  items-center flex-row">
-               <div>
-               <p className="text-2xl font-bold">{name}</p>
-                <p>{quantity}</p>
-                <p>{supplier}</p>
-                <p>{taste}</p>
-               </div>
+                <div>
+                    <p className="text-2xl font-bold">{name}</p>
+                    <p>{quantity}</p>
+                    <p>{supplier}</p>
+                    <p>{taste}</p>
+                </div>
                 <div className="card-actions justify-end">
                     <div className="join join-vertical space-y-4">
                         <button className="btn join-item">View</button>
                         <Link to={`updateCoffee/${_id}`}>
-                        <button className="btn join-item">Edit</button>
+                            <button className="btn join-item">Edit</button>
                         </Link>
-                        <button onClick={()=> handleDelete(_id)} className="btn join-item ">X</button>
+                        <button onClick={() => handleDelete(_id)} className="btn join-item ">X</button>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-
+CoffeeCard.propTypes = {
+    coffee: PropTypes.node,
+    coffees: PropTypes.node,
+    setCoffees: PropTypes.node,
+}
 export default CoffeeCard;
